66a1d6faab32cc67c9ac0ca72a8771d168a32584
Nikita Lutoshkin <n28280202@gmail.com>
2021-01-29 20:49:41 +0300

    Обьявлена и выведена в консоль переменная budgetDay

--- lesson/script.js
+++ lesson/script.js
@@ -4,6 +4,7 @@ let addExpenses = 'Интернет, Такси, Коммуналка';
 let deposit = Boolean(0);
 let mission = 100000;
 let period = 12;
+let budgetDay = money / 30;
 
 console.log('money: ', typeof(money));
 console.log('income: ', typeof(income));
@@ -11,3 +12,4 @@ console.log('deposit: ', typeof(deposit));
 console.log('addExpenses: ', addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log(addExpenses.toLowerCase().split(', '));
+console.log('budgetDay: ', budgetDay);
