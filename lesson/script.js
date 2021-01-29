2ba4556045d8e5794bf0f792a4e278159ed04536
Nikita Lutoshkin <n28280202@gmail.com>
2021-01-29 20:46:45 +0300

    Добавлены выводы переменных в консоль

--- lesson/script.js
+++ lesson/script.js
@@ -1,6 +1,13 @@
 let money = 30000;
 let income = 'фриланс';
-let addExpenses = 'интернет, такси, коммуналка';
-let deposit = true;
+let addExpenses = 'Интернет, Такси, Коммуналка';
+let deposit = Boolean(0);
 let mission = 100000;
 let period = 12;
+
+console.log('money: ', typeof(money));
+console.log('income: ', typeof(income));
+console.log('deposit: ', typeof(deposit));
+console.log('addExpenses: ', addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log(addExpenses.toLowerCase().split(', '));
