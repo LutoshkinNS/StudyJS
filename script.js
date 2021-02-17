4c382287e91c63f759bc9b061f499672a76af2ec
Nikita Lutoshkin <n28280202@gmail.com>
2021-02-17 17:16:47 +0300

    lesson14_part1 ready to check

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-'use strict'
+'use strict';
+
+function DomElement(selector, height, width, bg, fontSize) {
+	this.selector = selector;
+	this.height = height;
+	this.width = width;
+	this.bg = bg;
+	this.fontSize = fontSize;
+};
+
+DomElement.prototype.checkString = function () {
+	if (this.selector[0] === '.') {
+		let div = document.createElement('div');
+		div.className = (this.selector.slice(1));
+		div.style.height = this.height;
+		div.style.width = this.width;
+		div.style.backgroundColor = this.bg;
+		div.style.fontSize = this.fontSize;
+		div.innerHTML = 'div с классом ' + '"' + this.selector.slice(1) + '"';
+		document.body.append(div);
+	};
+	if (this.selector[0] === '#') {
+		let p = document.createElement('p');
+		p.id = this.selector.slice(1);
+		p.style.height = this.height;
+		p.style.width = this.width;
+		p.style.backgroundColor = this.bg;
+		p.style.fontSize = this.fontSize;
+		p.innerHTML = 'p с id ' + '"' + this.selector.slice(1) + '"';
+		document.body.append(p);
+	};
+};
+
+let newDomElement = new DomElement('#title', '50px', '400px', 'green', '36px');
+
+newDomElement.checkString();
