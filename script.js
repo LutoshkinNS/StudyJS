22362de01811adabb3246df5000068e549a7b359
Nikita Lutoshkin <n28280202@gmail.com>
2021-02-18 14:22:48 +0300

    lesson 15 part 2 ready to check

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict'
+
+class First {
+
+	hello () {
+		return console.log('Привет я метод родителя!');
+	};
+};
+
+
+class Second extends First {
+	constructor() {
+		super();
+		super.hello();
+	};
+
+	hello () {
+		return console.log('A я наследуемый метод!');
+	};
+};
+
+const second = new Second();
+
+second.hello();
