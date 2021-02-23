fcbdda38c5a64face2169487f40efab847db467a
Nikita Lutoshkin <n28280202@gmail.com>
2021-02-23 11:37:55 +0300

    added timer

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-'use strict'
+document.addEventListener('DOMContentLoaded', () => {
+
+	const countTimer = deadline => {
+		const timerHours = document.getElementById('timer-hours'),
+			timerMinutes = document.getElementById('timer-minutes'),
+			timerSeconds = document.getElementById('timer-seconds');
+
+		const getTimeRemaining = () => {
+			const dateStop = new Date(deadline).getTime(),
+				dateNow = new Date().getTime(),
+				timeRemaining = (dateStop - dateNow) / 1000,
+				seconds = Math.floor(timeRemaining % 60),
+				minutes = Math.floor((timeRemaining / 60) % 60),
+				hours = Math.floor((timeRemaining / 60) / 60);
+			return { timeRemaining, hours, minutes, seconds };
+		};
+
+
+		const updateClock = () => {
+			const timer = getTimeRemaining();
+
+			timerHours.textContent = ('0' + timer.hours).slice(-2);
+			timerMinutes.textContent = ('0' + timer.minutes).slice(-2);
+			timerSeconds.textContent = ('0' + timer.seconds).slice(-2);
+
+			if (timer.timeRemaining <= 0) {
+				// eslint-disable-next-line no-use-before-define
+				clearInterval(startTimer);
+			}
+		};
+
+		const startTimer = setInterval(updateClock, 1000);
+
+	};
+
+	countTimer('24 feb 2021');
+
+});
